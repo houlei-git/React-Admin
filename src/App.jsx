9f2574dca41005538f06dae8c3ab21284ee7f3db
hou-lei <hou-lei@git.com>
2019-04-03 10:42:47 +0800

    'admin'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'antd'
+export default class App extends Component {
+
+  render() {
+    return (
+     <div>
+       <Button type='primary'>测试</Button>
+     </div>
+    )
+  }
+}
